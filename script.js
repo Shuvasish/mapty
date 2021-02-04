307a9f23097d634bda6e8f8bfd147ce8ce14e25b
shuvasish <shuvasishtalukdershuvoalways@gmail.com>
2021-02-04 14:53:18 +0600

    added map using leaflet lib

--- script.js
+++ script.js
@@ -10,3 +10,41 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+
+    const cords = [latitude, longitude];
+    // console.log(cords);
+
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    const map = L.map('map').setView(cords, 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    map.on('click', function (mapEvent) {
+      console.log(mapEvent.latlng.lat, mapEvent.latlng.lng);
+      L.marker([mapEvent.latlng.lat, mapEvent.latlng.lng])
+        .addTo(map)
+        .bindPopup(
+          L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup',
+          })
+        )
+        .setPopupContent('workout')
+        .openPopup();
+    });
+  },
+  function () {
+    alert('Could not get your position');
+  }
+);
