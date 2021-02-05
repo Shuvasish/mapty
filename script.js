f8dfe283c5a0507939f23e408ab49f38980ccd0d
shuvasish <shuvasishtalukdershuvoalways@gmail.com>
2021-02-05 15:43:35 +0600

    added 3more classes and input validation

--- script.js
+++ script.js
@@ -3,6 +3,48 @@
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat,lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    //min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+// const run1 = new Running([33, 33], 33, 32, 3);
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration, elevationGain);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    //km/hr
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+//////////////////////////
+//APPLICATION ARCHITECTURE
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
@@ -68,13 +110,43 @@ class App {
     inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
   }
   _neWorkout(e) {
+    //checking for number
+    const validInput = (...inputs) => inputs.every(inp => Number.isFinite(inp));
+
+    //checking for positive number
+    const allPositive = (...inputs) => inputs.every(inp => inp > 0);
+
+    //preventing reload on click
     e.preventDefault();
 
-    //clearing input fields
-    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value =
-      '';
+    //get data from form
+    const type = inputType.value;
+    const distance = +inputDistance.value;
+    const duration = +inputDuration.value;
 
-    //display marker
+    //if workout running, create running object
+    if (type === 'running') {
+      const cadence = +inputCadence.value;
+      //check if data is valid
+      if (
+        !validInput(distance, duration, cadence) ||
+        !allPositive(distance, duration, cadence)
+      )
+        return alert('number must be positive');
+    }
+    //if workout cycling, create cycling object
+    if (type === 'cycling') {
+      const elevation = +inputElevation.value;
+      //check if data is valid
+      if (
+        !validInput(distance, duration, elevation) ||
+        !allPositive(distance, duration)
+      )
+        return alert('number must be positive');
+    }
+    //add new object to the workout
+
+    //render workout on the map as a marker
     console.log(this.#mapEvent.latlng.lat, this.#mapEvent.latlng.lng);
     L.marker([this.#mapEvent.latlng.lat, this.#mapEvent.latlng.lng])
       .addTo(this.#map)
@@ -89,6 +161,17 @@ class App {
       )
       .setPopupContent('workout')
       .openPopup();
+
+    //render workout on the list
+
+    //hide the form and clear the input fields
+
+    //clearing input fields
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value =
+      '';
+
+    //display marker
+
     // form.classList.add('hidden');
   }
 }
